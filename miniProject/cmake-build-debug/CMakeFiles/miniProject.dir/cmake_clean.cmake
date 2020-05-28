28279c63d09fb282f1a9b70fdc89e5431bc835fa
Amit Hebbi <anhebbi@gmail.com>
2020-05-28 12:02:20 +0530

    code update

---
+++ miniProject/cmake-build-debug/CMakeFiles/miniProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/miniProject.dir/main.c.obj"
+  "libminiProject.dll.a"
+  "miniProject.exe"
+  "miniProject.exe.manifest"
+  "miniProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/miniProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
